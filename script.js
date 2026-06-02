1842165ecee1801116acfce0d61a3708b52240a7
thirukkumaran-student <thirukkumaran2410584@ssn.edu.in>
2026-06-02 15:02:09 +0530

    Calculator is completely finished

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ function len(input){
     return input.length;
 }
 function appendToDisplay(input){
+    if(display.textContent == '0' || display.textContent == 'Error'){
+        display.textContent = ''
+    }
     display.textContent += input;
 }
 
@@ -11,7 +14,37 @@ function clearDisplay(){
 }
 
 function deleteFromDisplay(){
-    display_length = len(display.textContent);
-    console.log(display_length);
+    let display_length = len(display.textContent);
     display.textContent = display.textContent.slice(0, display_length - 1);
+}
+
+function calculate(){
+    let disp = display.textContent;
+    if(isCorrectFormat(disp))
+    {
+        disp = disp.replaceAll("^", "**")
+        disp = disp.replaceAll("X", "*")
+        disp = disp.replaceAll("%", "/100")
+        display.textContent = eval(disp)
+    }
+}
+
+function isCorrectFormat(exp){
+    if(isNaN(Number(exp[exp.length - 1]))){
+        display.textContent = "Error"
+        // display.textContent = ''
+        return false
+    }
+    if(isNaN(Number(exp[0]))){
+        if(exp[0] == '-'){
+            return true
+        }
+        else{
+            display.textContent = "Error"
+            // display.textContent = ''
+            return false
+        }
+    }
+    return true;
+
 }
