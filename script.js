84aebe2c9264918a694a5d5763b0c71cd8478d47
thirukkumaran-student <thirukkumaran2410584@ssn.edu.in>
2026-06-02 11:48:16 +0530

    initial functionalities done

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-const button1 = document.getElementsByClassName('button');
-console.log(button1);
+const display = document.getElementById('digit-display');
+function len(input){
+    return input.length;
+}
+function appendToDisplay(input){
+    display.textContent += input;
+}
 
-const button2 = document.querySelectorAll('.button')
-console.log(button2)
+function clearDisplay(){
+    display.textContent = '';
+}
+
+function deleteFromDisplay(){
+    display_length = len(display.textContent);
+    console.log(display_length);
+    display.textContent = display.textContent.slice(0, display_length - 1);
+}
